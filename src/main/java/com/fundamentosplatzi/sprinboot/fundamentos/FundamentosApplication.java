22f7600228cff2345bb642049f12819e2bd4bcb6
Henry Alfonso Garzon Sanchez <halgarjr@gmail.com>
2022-03-22 13:23:07 -0500

    Se hace uso de las excepciones y los mensajes correspondientes Log

--- src/main/java/com/fundamentosplatzi/sprinboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/sprinboot/fundamentos/FundamentosApplication.java
@@ -3,6 +3,8 @@ package com.fundamentosplatzi.sprinboot.fundamentos;
 import com.fundamentosplatzi.sprinboot.fundamentos.Component.ComponentDependency;
 import com.fundamentosplatzi.sprinboot.fundamentos.bean.*;
 import com.fundamentosplatzi.sprinboot.fundamentos.pojo.UserPojo;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -12,37 +14,49 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
-	private ComponentDependency componentDependency;
-	private MyBean myBean;
-	private MyBeanWithDependency myBeanWithDependency;
-	private BeanMiAsignatura beanMiAsignatura;
-	private MyBeanWhitProperties myBeanWhitProperties;
-	private UserPojo userPojo;
+    private final Log LOGGER = LogFactory.getLog("FundamentosApplication.class");
+
+    private ComponentDependency componentDependency;
+    private MyBean myBean;
+    private MyBeanWithDependency myBeanWithDependency;
+    private BeanMiAsignatura beanMiAsignatura;
+    private MyBeanWhitProperties myBeanWhitProperties;
+    private UserPojo userPojo;
 
 
-	//public FundamentosApplication(@Qualifier("componentImplement") ComponentDependency componentDependency){
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, BeanMiAsignatura beanMiAsignatura, MyBeanWhitProperties myBeanWhitProperties, UserPojo userPojo){
-		this.componentDependency=componentDependency;
-		this.myBean=myBean;
-		this.myBeanWithDependency=myBeanWithDependency;
-		this.beanMiAsignatura=beanMiAsignatura;
-		this.myBeanWhitProperties=myBeanWhitProperties;
-		this.userPojo=userPojo;
-	}
+    //public FundamentosApplication(@Qualifier("componentImplement") ComponentDependency componentDependency){
+    public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, BeanMiAsignatura beanMiAsignatura, MyBeanWhitProperties myBeanWhitProperties, UserPojo userPojo) {
+        this.componentDependency = componentDependency;
+        this.myBean = myBean;
+        this.myBeanWithDependency = myBeanWithDependency;
+        this.beanMiAsignatura = beanMiAsignatura;
+        this.myBeanWhitProperties = myBeanWhitProperties;
+        this.userPojo = userPojo;
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(FundamentosApplication.class, args);
-	}
+        SpringApplication.run(FundamentosApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		componentDependency.saludar();
-		myBean.print();
-		myBeanWithDependency.printWithDependency();
-		beanMiAsignatura.imprimirMiAsignatura();
-		System.out.println(myBeanWhitProperties.function());
-		System.out.println(userPojo.getEmail()+ " - Password: " + userPojo.getPassword()+ " - Edad: " + userPojo.getAge());
+    @Override
+    public void run(String... args) throws Exception {
+        componentDependency.saludar();
+        myBean.print();
+        myBeanWithDependency.printWithDependency();
+        beanMiAsignatura.imprimirMiAsignatura();
+        System.out.println(myBeanWhitProperties.function());
+        System.out.println(userPojo.getEmail() + " - Password: " + userPojo.getPassword() + " - Edad: " + userPojo.getAge());
+        try {
+            //error
+            int value= 10/0;
+            LOGGER.info("Mi valor :"+ value);
+        } catch (Exception e) {
+//            LOGGER.error("Esto es un error del aplicativo");
+            LOGGER.error("Esto es un error de división por cero" + e.getMessage()+" - "+e.getStackTrace());
+        }
+//        LOGGER.error("Esto es un error del aplicativo");
 
-	}
+
+    }
 }
--- src/main/java/com/fundamentosplatzi/sprinboot/fundamentos/bean/MyBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentosplatzi/sprinboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -1,6 +1,11 @@
 package com.fundamentosplatzi.sprinboot.fundamentos.bean;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
+
+        Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImplement.class);
 
     private MyOperation myOperation;
 
@@ -11,7 +16,12 @@ public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
 
     @Override
     public void printWithDependency() {
+
+        LOGGER.info("Se ha ingresado al método printWithDependency de la clase MyBeanWithDependencyImplement");
+
         int numero=1;
+
+        LOGGER.debug("El número enviado como parámetro a la dependencia myOperation es "+numero);
         System.out.println(myOperation.sum(numero));
 
         System.out.println("Hola desde la implementación de un Bean con dependencia");
